029714a0bc1b11038dacb17a45f75c7fb315baab
bnadeem-taknik <64201828+bnadeem-taknik@users.noreply.github.com>
2023-08-21 21:14:28 +0500

    updated'

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -3,7 +3,7 @@ import Socials from "../Socials";
 import Link from "next/link";
 import Button from "../Button";
 
-const Footer = ({}) => {
+const Footer = ({ }) => {
   return (
     <>
       <div className="mt-5 laptop:mt-40 p-2 laptop:p-0">
@@ -16,7 +16,7 @@ const Footer = ({}) => {
             <h1 className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl text-bold">
               TOGETHER
             </h1>
-            <Button type="primary">Schedule a call</Button>
+            <Button type="primary">Feel free to whatsapp me @ +92 340 1518895</Button>
             <div className="mt-10">
               <Socials />
             </div>
@@ -24,9 +24,9 @@ const Footer = ({}) => {
         </div>
       </div>
       <h1 className="text-sm text-bold mt-2 laptop:mt-10 p-2 laptop:p-0">
-        Made With ❤ by{" "}
+        Remake With ❤ by{" "}
         <Link href="http://www.chetanverma.com">
-          <a className="underline underline-offset-1">Chetan Verma</a>
+          <a className="underline underline-offset-1">Hamza Akhtar</a>
         </Link>
       </h1>
     </>
